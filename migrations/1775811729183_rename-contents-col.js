723320aa72b955ad5eb79a58c126d845b14a2ee8
Richard Domander <>
2026-04-10 12:31:57 +0300

    Add migration

---
+++ migrations/1775811729183_rename-contents-col.js
@@ -0,0 +1,31 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+const oldColumnName = "contents";
+const newColumnName = "body";
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.sql(`
+        ALTER TABLE comments
+        RENAME COLUMN ${oldColumnName} TO ${newColumnName};
+    `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.sql(`
+        ALTER TABLE comments
+        RENAME COLUMN ${newColumnName} TO ${oldColumnName};
+    `);
+};
